d2b4732d41062a23e1c70783778d32000e7693a5
Jenga05 <80204216+Jenga05@users.noreply.github.com>
2023-04-16 18:17:34 +0200

    add tests for sensor

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,5 +1,7 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,13 +9,59 @@ import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    TrainSensorImpl sensor;
+    TrainController controller;
+    TrainUser user;
+
+    /**
+     * Initialize the component with mocks
+     */
     @Before
     public void before() {
-        // TODO Add initializations
+        controller = mock(TrainController.class);
+        user = mock(TrainUser.class);
+        sensor = new TrainSensorImpl(controller,user);
     }
 
+    /**
+     * Setting the speed above the absolute limit
+     * Expecting the user to be in alert state after that
+     */
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void SetSpeedAboveAbsoluteLimit() {
+        sensor.overrideSpeedLimit(501);
+        verify(user,times(1)).setInWarningState(true);
+    }
+
+    /**
+     * Setting the speed under the absolute limit
+     * Expecting the user to be in alert state after that
+     */
+    @Test
+    public void SetSpeedUnderAbsoluteLimit() {
+        sensor.overrideSpeedLimit(-1);
+        verify(user,times(1)).setInWarningState(true);
+    }
+
+    /**
+     * Setting the speed under the logical limit
+     * Expecting the user to be in alert state after that
+     */
+    @Test
+    public void SetSpeedSlowerThatLimit() {
+        when(controller.getReferenceSpeed()).thenReturn(20);
+        sensor.overrideSpeedLimit(5);
+        verify(user,times(1)).setInWarningState(true);
+    }
+
+    /**
+     * Setting the speed within all the limits
+     * Expecting the user not to be in alert state after that
+     */
+    @Test
+    public void SetSpeedInLimit() {
+        when(controller.getReferenceSpeed()).thenReturn(20);
+        sensor.overrideSpeedLimit(15);
+        Assert.assertFalse(user.isInWarningState());
     }
 }
